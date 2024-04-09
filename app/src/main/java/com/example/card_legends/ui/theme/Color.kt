d16872fb2ffde34c7e417098447855d80c4b063b
admin <admin@Renan.local>
2024-04-09 20:00:47 -0300

    first commit

---
+++ app/src/main/java/com/example/card_legends/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.card_legends.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//APP COLORS
+val Purple100 = Color(0xFF292738)
+val Purple200 = Color(0xFF363447)
+val Yellow = Color(0xFFdfb160)
+val GrayDark = Color(0xFF273444)
+val Gray = Color(0xFF8492a6)
+val GrayLight = Color(0xFFd3dce6)
